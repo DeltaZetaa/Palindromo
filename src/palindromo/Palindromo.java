d01af2c03c4abebe2e14413d6ebe3c04d8744695
DeltaZetaa <153007810+DeltaZetaa@users.noreply.github.com>
2024-03-18 16:45:52 +0100

    Update Palindromo.java

--- src/palindromo/Palindromo.java
+++ src/palindromo/Palindromo.java
@@ -8,34 +8,25 @@ public class Palindromo {
 	}
 
 	private static void detectorDePalindromos(String palindromo) {
-		String reverso = "";
-		String anverso = "";
-		
 		
 		char[] anver = palindromo.toCharArray();
-		for(int i = 0; i<palindromo.length(); ++i ) {
-			System.out.println(i + " " + anver[i]);
-		}
-		
-		
 		System.out.println("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
 		
 		char[] rever = new char[palindromo.length()];
 		int x = 0;
 		for(int i = palindromo.length() -1; i>-1; --i, ++x) {
 			 rever[x] = palindromo.charAt(i);
-			System.out.println(x + " " + rever[x]);
 		}
-		
+	
 		System.out.println("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+		
+		String reverso = "";
+		String anverso = "";
 		reverso = String.valueOf(rever);
-		System.out.println("Palindromo al reves: "+reverso);
-		
 		anverso = String.valueOf(anver);
-		System.out.println("Palindromo del derecho: "+anverso);
+		
 		System.out.println("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
 		
-		//IF DE COMPROBACION▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 		if(reverso.equals(anverso)) {
 			System.out.println("Es un Palindromo");
 		}
